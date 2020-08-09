b9b3d1b4eae174257606e00066191ec8567cf326
Yekaterina Karakulina <ekaterina.jj@gmail.com>
2020-08-09 13:07:50 +0400

    feat: main file structure, react+redux project structure

--- src/services/bookstore-service.js
+++ src/services/bookstore-service.js
@@ -1,6 +1,18 @@
 export default class BookstoreService {
 
   getBooks() {
-    return [];
+    return [
+      {
+        id: 1,
+        title: 'Production-Ready Microservices',
+        author: 'Susan J. Fowler'
+      },
+      {
+        id: 2,
+        title: 'Release it!',
+        author: 'Michael T. Nygard'
+      }
+
+    ];
   }
 }

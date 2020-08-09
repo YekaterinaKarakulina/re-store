9e7108a8cae948d1adf8753df049099eccb53b63
Yekaterina Karakulina <ekaterina.jj@gmail.com>
2020-08-09 16:34:52 +0400

    feat: add shop-header, shopping-csrt-table components, add some styles

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
+import ShopHeader from '../shop-header';
+import ShoppingCartTable from '../shopping-cart-table';
 import { withBookstoreService } from '../hoc';
 import { HomePage, CartPage } from '../pages';
 
 const App = () => {
   return (
-    <Switch>
-      <Route path="/" component={HomePage} exact />
-      <Route path="/cart" component={CartPage} />
-    </Switch>
+    <main role="main" className="container">
+      <ShopHeader numItems={5} total={210} />
+      <Switch>
+        <Route path="/" component={HomePage} exact />
+        <Route path="/cart" component={CartPage} />
+      </Switch>
+      <ShoppingCartTable />
+    </main>
   )
 }
 
